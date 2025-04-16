ec235497678de5893e7328dee5fe38c5941f9d4f
Júlia Martins Monteiro <juliamamonteiro02@gmail.com>
2025-04-16 14:33:11 -0300

    deleted:    home/css/img/image.png 	deleted:    home/css/img/logo.png 	modified:   src/App.jsx 	renamed:    home/css/img/Down Arrow.png -> src/assets/Down Arrow.png 	renamed:    home/css/img/Pin.png -> src/assets/Pin.png 	renamed:    home/css/img/Report.png -> src/assets/Report.png 	renamed:    home/css/img/Vector.png -> src/assets/Vector.png 	renamed:    home/css/img/busca.png -> src/assets/busca.png 	renamed:    home/css/img/evidencia.jpg -> src/assets/evidencia.jpg 	renamed:    home/css/img/file.png -> src/assets/file.png 	renamed:    home/css/img/filtro.png -> src/assets/filtro.png 	renamed:    home/css/img/folder.png -> src/assets/folder.png 	renamed:    home/css/img/foto.png -> src/assets/foto.png 	renamed:    home/css/img/home.png -> src/assets/home.png 	new file:   src/componentes/MenuLateral.jsx 	new file:   src/componentes/MenuLateral.module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+
 import { useAuth } from "./context/AuthContext";
 
 import './global.css';
@@ -19,7 +20,12 @@ function App() {
         element={!token ? <Login /> : <Navigate to="/" replace />}
       />
       <Route path="*" element={<Navigate to="/" />} />
+      <Route
+      path="/home"
+      element={token ? <Home /> : <Navigate to="/login" replace />}
+      />
     </Routes>
+    
   );
 }
 
