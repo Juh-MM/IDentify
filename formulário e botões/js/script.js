a8447052da363304142d34ec6c88f71fad1ec5f9
Vivian  Brito <165101632+viviangbrito@users.noreply.github.com>
2025-04-18 13:02:09 -0300

    Update script.js

--- formulário e botões/js/script.js
+++ formulário e botões/js/script.js
@@ -49,7 +49,7 @@
     // === ADICIONAR EVIDÊNCIA (LOCAL) ===
     const inputEvidencia = document.getElementById("inputEvidencia");
     const listaEvidencias = document.getElementById("lista-evidencias");
-    const mensagemDiv = document.getElementById("mensagem");
+    //const mensagemDiv = document.getElementById("mensagem");
     const tabelaEvidenciasBody = document.querySelector("#tabelaEvidencias tbody");
     const evidenciasLocal = [];
   
@@ -82,9 +82,9 @@
           };
           reader.readAsDataURL(file);
   
-          mensagemDiv.style.display = "block";
-          mensagemDiv.style.color = "green";
-          mensagemDiv.textContent = "Evidência adicionada localmente!";
+        // mensagemDiv.style.display = "block";
+        //mensagemDiv.style.color = "green";
+        //mensagemDiv.textContent = "Evidência adicionada localmente!";
         });
   
         event.target.value = ''; // Resetar input
